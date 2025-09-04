b43a0086493730d897fae8d11641ceb3378f6c99
Arthur Fernandes Vaz <155276471+arthurvaz18@users.noreply.github.com>
2025-09-04 00:55:12 -0300

    Configurações de ambiente e docker atualizadas

---
+++ src/main/java/com/localsense/localSense/LocalSenseApplication.java
@@ -0,0 +1,13 @@
+package com.localsense.localSense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalSenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localsense/localSense/LocalSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localsense.localSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
